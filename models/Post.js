4de9095f32cd14257a4d775e8d3e4fdba4dc32df
SpecsNStats <specsnstats@gmail.com>
2021-10-28 18:22:14 -0600

    post model

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+const {Model, DataTypes} = require("sequelize")
+const sequelize = require('../config/connection');
+const bcrypt = require("bcrypt");
+
+class Post extends Model {}
+
+Post.init({
+    // add properites here, ex:
+    title: {
+         type: DataTypes.STRING,
+         unique:true,
+         validate:{
+            isAlphanumeric:true
+         }
+    },
+    text:{
+        type: DataTypes.TEXT,
+        validate:{
+        }
+    },
+},
+    sequelize
+);
+
+module.exports=User
