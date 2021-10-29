e350e4e1f4e978f6554349088c2a1e0512e209f3
SpecsNStats <specsnstats@gmail.com>
2021-10-28 18:06:51 -0600

    added index and user models

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const {Model, DataTypes} = require("sequelize")
+const sequelize = require('../config/connection');
+const bcrypt = require("bcrypt");
+
+class User extends Model {}
+
+User.init({
+    // add properites here, ex:
+    username: {
+         type: DataTypes.STRING,
+         unique:true,
+         validate:{
+            isAlphanumeric:true
+         }
+    },
+    password:{
+        type:DataTypes.STRING,
+        validate:{
+            len:[8]
+        }
+    },
+},{
+    hooks:{
+        beforeCreate(newUser){
+            newUser.username = newUser.username.toLowerCase();
+            newUser.password = bcrypt.hashSync(newUser.password,5);
+            return newUser;
+        },
+        beforeUpdate(updatedUser){
+            updatedUser.username = updatedUser.username.toLowerCase();
+            updatedUser.password = bcrypt.hashSync(updatedUser.password,5);
+            return updatedUser;
+        }
+    },
+    sequelize,
+});
+
+module.exports=User
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sequelize = require("./config/connection.js")
+const sequelize = require("./config/connection.js");
 const session = require("express-session");
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
