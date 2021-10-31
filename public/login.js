f23ad03358da9c362648f0197ba31371ad1204b7
SpecsNStats <specsnstats@gmail.com>
2021-10-31 13:30:58 -0600

    fixed another login issue, adding signup

--- public/login.js
+++ public/login.js
@@ -1,25 +1,48 @@
-async function loginUser(event) {
-    event.preventDefault();
-    const loginUsername = document.querySelector("#login-username").value
-    const loginPassword = document.querySelector("#login-password").value
-    console.log("submitted username is " + loginUsername)
-    console.log("submitted password is " + loginPassword)
+const loginForm = document.getElementById("login")
+const signupForm = document.getElementById("signup")
 
-    const response = await fetch(`api/users/login`, {
-        method:"POST",
-        body: JSON.stringify({
-            loginUsername,
-            loginPassword
-        }),
-        headers:{
-            "Content-Type":"application/json"
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const loginUsername = document.getElementById('login-username').value
+    const loginPassword = document.getElementById('login-password').value
+
+    if(loginUsername && loginPassword){
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                username: loginUsername, 
+                password: loginPassword 
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        if(response.ok){
+            console.log(response);
+            location.replace('/profile')
+        } else {
+            alert('YOU ENTERED THE WRONG INFORMATION')
         }
-    })
-    if (response.ok){
-        document.location.replace("/profile")
-    } else{
-        alert("Failed to Login")
     }
-}
+});
 
-document.querySelector("#login").addEventListener("submit", loginUser)
+signupForm.addEventListener("submit", async (event)=>{
+    event.preventDefault()
+    const signupUsername = document.getElementById("signup-Username").value
+    const signupPassword = document.getElementById("signup-Password").value
+
+    if(signupUsername && signupPassword){
+        const response = await fetch("/api/users", {
+            method: "POST",
+            body: JSON.stringify({
+                username: signupUsername,
+                password: signupPassword
+            }),
+            headers: {"Content-Type": "application/json"}
+        })
+        if(response.ok){
+            location.href= "/profile"
+        } else{
+            alert("Username already exists, please login!")
+        }
+    }
+})
